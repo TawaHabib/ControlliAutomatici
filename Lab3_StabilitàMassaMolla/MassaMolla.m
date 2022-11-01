508047ab328a8f72ab0c933312c7895681b76694
TawaHabib <tawadrosgirgis.habib01@universitadipavia.it>
2022-11-01 17:00:23 +0100

    comment

--- Lab3_StabilitàMassaMolla/MassaMolla.m
+++ Lab3_StabilitàMassaMolla/MassaMolla.m
@@ -32,17 +32,19 @@ x=[0.5 0;1 0; 2 0]
 for i=1:3
 
     x0=x(i,:)'
-    [X,U,Y] = trim('SF_MassaMolla',x0,[],[],1)
-    [Al,Bl,Cl,Dl] = linmod('SF_MassaMolla',X,U)
+    [X,U] = trim('SF_MassaMolla',x0,[],[],1);
+    [Al,Bl,Cl,Dl] = linmod('SF_MassaMolla',X,U);
     eig(Al) 
     pause
 end
-
+%Consclisioni
+%1-ass stabile
+%2-nan
+%3.instabile
 
 %% 2.2-2.3-confronto risposta all scalino
 c=1.1;
-x=[0.5 0;1 0; 2 0]
-x0=x(1,:)'
+x0=[0.5 0]';
 [X,U,Y] = trim('SF_MassaMolla',x0,[],[],1);
 [Al,Bl,Cl,Dl] = linmod('SF_MassaMolla',X,U);
 
