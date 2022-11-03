e286ff9b3ed72459a4c223a47340bebcb5b516ca
TawaHabib <tawadrosgirgis.habib01@universitadipavia.it>
2022-11-03 17:38:27 +0100

    done lab 4

--- Lab4_SistemaCompartimentale/CompSys.m
+++ Lab4_SistemaCompartimentale/CompSys.m
@@ -1,7 +1,9 @@
 clear
 clc
 close all
+
 %% 1.0-Inizializzazione
+
 k1=1;
 k2=0.5;
 b1=0.5;
@@ -16,23 +18,28 @@ sys=ss(A,B,C,D);
 
 %% 1.1-Raggiungibilità con solo con inezione in vena
 
-% ctrb(sys)
 rank(ctrb(sys))
+
 %Rank(marice di raggiungibilita')<2 --> sistema non completamente
 %raggiungibile, solo la x2 punto lo è
+
 %% 1.2 solo in vene 
+
 %% 1.2.1 continuo
+
 uc=(0.5*k2/b2);
 [YC,TC,XC]=step(sys*uc);
 
 %% 1.2.2-Iniezioni ad intervalli regolari
-%tra 0.45-0.55
+
+% tra 0.45-0.55
 % deltaImpulsi=(log((0.55/0.45))/(k2));
 % ampiezzaImpulsi=(exp(k2*deltaImpulsi)*0.45/(5.5*b2));
 
-%tra 0.49-0.53
+% tra 0.49-0.53
 deltaImpulsi=-(log((0.49/0.53))/(k2));
 ampiezzaImpulsi=(exp(k2*deltaImpulsi)/(27*b2));
+
 numeroImpulsi=100;
 
 t = linspace(0,deltaImpulsi,10);
@@ -73,11 +80,13 @@ title('Concentrazione nel sangue iniezione continua')
 legend('farmaco nel sangue','limite')
 
 %% pulisco
+
 clear
 clc
 close all
 
 %% 2.0-Inizializazzione
+
 clc
 k1=1;
 k2=0.5;
@@ -90,16 +99,18 @@ C=[0 1];
 D=0;
 
 sys=ss(A,B,C,D);
+
 %% 2.1-raggiungibilità ingestione
 
-% ctrb(sys)
 rank(ctrb(sys))
+
 %Rank(marice di raggiungibilita')=2 --> sistema  completamente
 %raggiungibile
+
 %% 2.2-determinazione sperimentale
 deltaImpulsi=1.8220;
 ampiezzaImpulsi=0.9381;
-numeroImpulsi=100;
+numeroImpulsi=20;
 
 t = linspace(0,deltaImpulsi,10);
 
@@ -117,6 +128,7 @@ for i = 1:numeroImpulsi
 end
 
 %% 2.3-Grafici e vincoli
+
 figure(1)
 plot(T,Y)
 hold on 
@@ -128,19 +140,25 @@ title('Concentrazione nel sangue pillole ad intervalli regolari')
 legend('farmaco nel sangue','limite')
 
 %% 2.4-Osservabilità del sistema
+
 rank(obsv(sys))
+
 %rango matrice osservabilità =2 ==> sistema completamente osservabile 
+
 %% 2.5-osservabilità se cambia la trasformazione d'uscita 
+
 %posso concludere che lo stato x1 diventerebbe osservabite mentre lo stato
 %x2 non lo diventerebbe per il raggionamento iniziale che ha fatto il prof
 %ragionamento (prima delle def di oss.tà):
-%   ________________             ___________________ 
-%  |     x1(dot)    |---x1----> |      x2(dot)      |
-%   ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾    |        ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾
-%                       #--->y
+%  _________________________schema_______________________
+% |   ________________             ___________________   |
+% |  |     x1(dot)    |---x1----> |      x2(dot)      |  |
+% |   ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾    |        ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾   |
+% |                      #--->y                          |
+%  ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾ 
 %ragionamento senza fare calcoli. facendo quindi i calcoli dovremmo
 %ottenere che il rango della matrice osservabilità deve fare 1 e non piu 2:
+
 C=[1 0];
 sys=ss(A,B,C,D);
 rank(obsv(sys))
-%% non ho capito cosa si intente per cosa si puo dedurre!
